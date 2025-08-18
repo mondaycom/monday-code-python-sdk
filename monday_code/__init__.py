9e8582a38cfd7febf6bf62cd80c45cb29c87b5b7
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2025-08-18 12:53:23 +0300

    bump to version 1.0.0 (#14)
    * reserve version 1
    
    * fix import

--- monday_code/__init__.py
+++ monday_code/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
 # import apis into sdk package
 from monday_code.api.environment_variables_api import EnvironmentVariablesApi
